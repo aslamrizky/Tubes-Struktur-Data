da399bea294d844331e8b0be7919d45bb452574e
Aslam Rizky <146598910+aslamrizky@users.noreply.github.com>
2024-12-28 19:54:04 +0700

    Update Train.h
    revisi code menjadi lebih sederhana

--- tubes-std/Train.h
+++ tubes-std/Train.h
@@ -1,35 +1,48 @@
 #ifndef TRAIN_H
 #define TRAIN_H
 
-#include <iostream>
-#include <vector>
-#include <map>
 #include <string>
-#include <queue>
+#include <vector>
 #include <algorithm>
-#include <limits>
 
+#define namaStasiun(s) s->namaStasiun
+#define stasiunSetelah(s) s->stasiunSetelah
+#define edgeStasiunPertama(s) s->edgeStasiunPertama
+#define destNamaStasiun(e) e->destNamaStasiun
+#define jarak(e) e->jarak
+#define edgeStasiunSetelah(e) e->edgeStasiunSetelah
+#define stasiunPertama(G) G.stasiunPertama
+
+#include <iostream>
 using namespace std;
 
-// Struktur untuk Edge (koneksi antar stasiun)
-struct Edge {
-    string tujuan;
+typedef struct stasiun *adrStasiun;
+typedef struct edgeStasiun *adrEdgeStasiun;
+
+struct stasiun {
+    string namaStasiun;
+    adrStasiun stasiunSetelah;
+    adrEdgeStasiun edgeStasiunPertama;
+};
+
+struct edgeStasiun {
+    string destNamaStasiun;
     int jarak;
+    adrEdgeStasiun edgeStasiunSetelah;
 };
 
-// Struktur untuk Vertex (stasiun)
-struct Vertex {
-    string nama;
-    vector<Edge> koneksi;
+struct graph {
+    adrStasiun stasiunPertama;
 };
 
-void tambahVertex(map<string, Vertex>& vertices, const string& nama);
-void tambahEdge(map<string, Vertex>& vertices, const string& asal, const string& tujuan, int jarak);
-void tampilkanSemuaRute(map<string, Vertex>& vertices);
-void cariRuteTercepat(const map<string, Vertex>& vertices, const string& asal, const string& tujuan);
-void tambahStasiun(map<string, Vertex>& vertices, const string& nama, const vector<pair<string, int>>& koneksi);
-void hapusStasiun(map<string, Vertex>& vertices, const string& nama);
-void cariSemuaRute(const map<string, Vertex>& vertices, const string& asal, const string& tujuan, vector<string>& ruteSaatIni, vector<vector<string>>& semuaRute, map<string, bool>& visited);
-void tampilkanSemuaRuteAlternatif(const map<string, Vertex>& vertices, const string& asal, const string& tujuan);
+void tambahStasiunBaru(string stasiunBaru, adrStasiun &s);
+void initStasiun(graph &G);
+void tambahStasiun(graph &G, string stasiunBaru);
+void tampilkanRute(graph &G);
+adrStasiun cariStasiun(graph &G, string stasiunDiCari);
+void tambahEdgeStasiun(graph &G, string stasiunSumber, string destNamaStasiun, int jarak);
+bool cariJalurDFS(graph &G, string sumber, string tujuan, vector<string> &path, int &totalJarak, vector<string> &bestPath, int &minNodes);
+void jalurTercepat(graph &G, vector<string> &bestPath, int &minNodes);
+void hapusStasiun(graph &G, string stasiunDiHapus);
 
 #endif // GRAPH_H
